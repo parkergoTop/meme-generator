2abef0c50b4567a65b7226b72b9e6d0cc9e007bd
Qingwen Xiao <parkerciao@gmail.com>
2022-09-09 18:07:02 +1000

    add state to generate the random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./Meme.css"
+import memesData from "./memesData"
 
 export default function Meme(){
+
+
+    
+    const [meme, setMeme] = React.useState({
+            topText : "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme(preMeme => ({...preMeme, randomImage:url}))
+    } 
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className ="form-input" value ="Top Text"/>
                 <input type="text" className ="form-input" value ="Button Text"/>
-                <input type="submit" className ="" value ="Get a meme image"/>
-            </form>
+                <button className ="form-button" onClick={getMemeImage}>Get a meme image</button>
+            </div>
+            <img src={meme.randomImage} className="meme-image" />
         </main>
     )
 }
